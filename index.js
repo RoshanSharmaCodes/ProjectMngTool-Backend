0760a406cced614ff42ce0addda7757a9a5d02c7
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2023-11-04 00:14:17 +0530

    Route for Create &  Download Report Is Added.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const pdf = require("html-pdf")
 const cors = require("cors")
 const bodyParser = require("body-parser")
-
+const pdfTemplate = require("./Pages/Report")
 const app = express()
 const port = 5000
 
@@ -12,6 +12,20 @@ app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.send({"message": "Your Server is Up and Running..."}).statusCode(500)
+})
+
+app.post("/createReport", (req, res) => {
+    pdf.create(pdfTemplate(),{}).toFile("Report.pdf",(err)=>{
+        if(err) {
+            return Promise.reject(err)
+        }
+        return Promise.resolve()
+    })
+    res.send({"message": "PDF Generated"})
+})
+
+app.get("/getReport", (req,res)=>{
+    res.sendFile(`${__dirname}/Report.pdf`)
 })
 
 app.listen(port,()=>{
