ae6ffb0c07663615455c9dace2daa701328b0220
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2023-11-16 21:46:49 +0530

    Projects Route Added.

---
+++ Routes/project.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const projectRouter = require("project")
+
+projectRouter.get("/projectDetails", (req,res)=>{
+
+})
+
+projectRouter.post("/projectCreate", (req,res)=>{
+
+})
+
+projectRouter.post("/projectDelete", (req,res)=>{
+
+})
+
+projectRouter.post("/projectUpdate", (req,res)=>{
+
+})
+
+projectRouter.post("/projectTaskAdd", (req,res)=>{
+
+})
+
+projectRouter.post("/projectTaskDelete", (req,res)=>{
+
+})
+
+projectRouter.post("/projectTaskUpdate", (req,res)=>{
+
+})
+
+projectRouter.get("/projectTaskDetails", (req,res)=>{
+
+})
+
+module.exports = projectRouter
