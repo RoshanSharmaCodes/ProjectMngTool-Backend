edbab9ecb0df80298b60af3d551dd469d2bf28f5
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2023-11-16 21:41:01 +0530

    Authentication Routes Added.

---
+++ Routes/authentication.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const authRouter = express.Router()
+
+authRouter.get("/login",(req,res)=>{
+
+})
+
+authRouter.post("/register", (req,res)=>{
+
+})
+
+authRouter.get("/forgotPassword", (req,res)=>{
+    
+})
+
+module.exports = authRouter
+
+
+
